6ca6112deb61b52f181cffed8fd4d44f15809984
Mattias Ernelli (mate18) <mattias.ernelli@teliasonera.com>
2016-02-05 11:59:57 +0100

    Added select event loop, work in progress

--- src/igmpclient.c
+++ src/igmpclient.c
@@ -1,10 +1,10 @@
 #include<sys/types.h>
-
 #include<sys/socket.h>
 #include <netinet/in.h>
-
 #include<sys/time.h>
+#include<sys/select.h>
 
+#include<unistd.h>
 #include<errno.h>
 #include<stdio.h>
 #include<string.h>
@@ -56,21 +56,22 @@ typedef void (*callback_t)(void *);
 typedef long long timestamp;
 
 struct timer_t {
-timestamp time;
-callback_t cb;
+  timestamp time;
+  callback_t cb;
+  void *closure;
 };
 
 struct timer_t timers[MAX_TIMERS];
 int num_timers = 0;
 
 timestamp getCurrentTime() {
-struct timeval time;
-gettimeofday(&time, NULL);
-
-return (timestamp)time.tv_sec*1000 + (timestamp)time.tv_usec/1000;
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  
+  return (timestamp)time.tv_sec*1000 + (timestamp)time.tv_usec/1000;
 }
 
-int setTimeout(callback_t cb, timestamp timeout) {
+int setTimeout(callback_t cb, timestamp timeout, void *closure) {
   if(num_timers >= MAX_TIMERS) {
     fprintf(stderr, "setTimeout failed, MAX_TIMERS: %d used\n", MAX_TIMERS);
     exit(1);
@@ -79,7 +80,7 @@ int setTimeout(callback_t cb, timestamp timeout) {
   if(timeout < 0) {
     timeout = 0;
   }
-
+  
   timestamp now = getCurrentTime();
   
   timestamp timeoutTime = now + timeout;
@@ -88,16 +89,38 @@ int setTimeout(callback_t cb, timestamp timeout) {
     i++;
   }
   
+  //insert timer
   memmove(timers+i+1, timers+i, sizeof(timers[0])*(num_timers-i));
   timers[i].time = timeoutTime;
   timers[i].cb = cb;
+  timers[i].closure = closure;
   num_timers++;
-
-
+  
+  
   return i;
 }
 
-void dumpTimers() {
+void _clearTimeout(int index) {
+  //remove timer
+  memmove(timers+index, timers+index+1, sizeof(timers[0])*(num_timers-index));
+  num_timers--;
+}
+
+timestamp _nextTimeout() {
+  timestamp next = -1;
+  
+  if(num_timers > 0) {
+    next = timers[0].time - getCurrentTime();
+    
+    if(next < 0) {
+      next = 0;
+    }
+  } 
+  return next;
+}
+
+
+void _dumpTimers() {
   printf("num timers: %d\n", num_timers);
   for(int i = 0; i < num_timers; i++) {
     printf("timer: %d, timeout: %Ld, callback: %016Lx\n", i, timers[i].time, (long long)timers[i].cb);
@@ -107,36 +130,80 @@ void dumpTimers() {
 
 int main(int argc, char *argv[]) {
   unsigned char buffer[2048];
+  timestamp _dt;
   int len;
+  int run = 1;
+
 
   printf("Create raw socket listen to igmp: 0x%04x\n", IPPROTO_IGMP);
 
-  int cb(void *c) { printf("timer 3 kicked\n"); return len; }
+  void cb(void *c) { 
+    printf("timer %s kicked\n", (const char*)c); 
+  }
 
-  setTimeout((void *)1, 400);
-  setTimeout((void *)2, 300);
-  setTimeout(cb, 900);
-  dumpTimers();
+  setTimeout(cb, 400, "second");
+  setTimeout(cb, 300, "first");
+  setTimeout(cb, 900, "last");
+
 
   int fd = socket(AF_INET, SOCK_RAW, IPPROTO_IGMP);
+  printf("got socket: %d\n", fd);
 
   if(fd > 0) {
-
-    printf("got socket: %d\n", fd);
-
     
+    fd_set rfds;
+    fd_set wfds;
+    fd_set efds;
 
+    FD_ZERO(&rfds);
 
-    while(1) {
-      len = recv(fd, buffer, sizeof(buffer), 0);
+    FD_SET(fd, &rfds);
+
+    while(run) {
+      _dt = _nextTimeout();
+    
+      struct timeval tv;      
       
-      if(len > 0) {
-        printf("got igmp packet:\n");
-        hexdump(buffer, len);
+      if(_dt > 0) {
+        tv.tv_sec = _dt / 1000;
+        tv.tv_usec = 1000*(_dt % 1000);
       } 
+
+      printf("select with timeout: %d %d\n", tv.tv_sec, tv.tv_usec);
+
+
+      FD_ZERO(&wfds);
+      FD_ZERO(&efds);
+
+
+
+      int retval = select(fd+1, &rfds, &wfds, &efds, _dt > 0 ? &tv : NULL);
+
+      if(retval == -1) {
+        perror("select()");
+        exit(1);
+      }
+
+      printf("select returned: %d\n", retval);
+
+      if(FD_ISSET(fd,&rfds)) {
+        len = recv(fd, buffer, sizeof(buffer), 0);
+        if(len > 0) {
+          printf("got igmp packet:\n");
+          hexdump(buffer, len);
+        } 
+        FD_CLR(fd, &rfds);
+      }
+
+      // invoke expired timers
+      while(_nextTimeout() == 0) {
+        timers[0].cb(timers[0].closure);
+        _clearTimeout(0);
+      }
+      
     }
   } else {
-    printf("Failed to create socket: %s\n", strerror(errno));
+    perror("Failed to create socket\n");
   }
   return 0;
 }
